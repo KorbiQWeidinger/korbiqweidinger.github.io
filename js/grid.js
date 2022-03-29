9574351bc40f7579f4fe4e39256818cca270bf66
KorbiQWeidinger <korbi.weidinger2@gmail.com>
2022-03-30 00:35:17 +0200

    more

--- js/grid.js
+++ js/grid.js
@@ -1,14 +1,13 @@
 function init() {
-    console.log("init")
-    $('#markdown-display').load('/html/aboutme.html')
+    console.log('init')
+    load_external_html_into('markdown-display','/html/aboutme.html')
 }
 
 init()
 
 function load_external_html_into(id, external_html_filename) {
-    console.log(id)
     console.log(external_html_filename)
-    $(id).load(external_html_filename)
+    $('#' + id).load(external_html_filename)
 }
 
 function unselect_file_item(file_item_id) {
@@ -37,3 +36,62 @@ function change_selected_file_in_top(img_class, filename) {
     const top_text_div = $('#top-file-item-text')
     top_text_div.text(filename)
 }
+
+let tree_is_open = false
+
+function open_project_tree() {
+    const outer = $('#outer-grid-container')
+    const tree = $('#file-tree')
+    const template_areas = outer.css('grid-template-areas')
+    if(tree_is_open) {
+        outer.css('grid-template', 'auto auto minmax(0, 1fr) 30px / 40px 0 minmax(0, 1fr) 40px')
+        outer.css('grid-template-areas', template_areas)
+        tree.css('visibility', 'collapse')
+        tree_is_open = false
+        return
+    }
+    outer.css('grid-template', 'auto auto minmax(0, 1fr) 30px / 40px auto minmax(0, 1fr) 40px')
+    outer.css('grid-template-areas', template_areas)
+    tree.css('visibility', 'visible')
+    tree_is_open = true;
+}
+
+//TODO
+
+const file_tree_element = $('#file-tree')
+const top_file_path = $('#top-file-path')
+
+class File {
+    constructor(icon_css_class, file_name, html_content_filename) {
+        this.icon_css_class = icon_css_class
+        this.file_name = file_name
+        this.html_content_filename = html_content_filename
+    }
+
+    select() {
+
+    }
+
+    unselect() {
+
+    }
+
+    open() {
+
+    }
+
+    close() {
+
+    }
+}
+
+
+
+
+const about_me = new File('html-file-icon', 'about_me.html', 'aboutme.html')
+const contact_me = new File('class-icon', 'ContactMe.java', 'aboutme.html')
+
+
+const files = [about_me]
+
+const selected_file = about_me
