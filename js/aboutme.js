5be9363de9e3762f27c12f4a67bf5e36522f36bd
KorbiQWeidinger <korbi.weidinger2@gmail.com>
2022-03-30 19:13:18 +0200

    clean up

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,3 +1,8 @@
+function load_external_html_into(id, external_html_filename) {
+    console.log(external_html_filename)
+    $('#' + id).load(external_html_filename)
+}
+
 function open_email_form() {
     $('#email-popup').css('visibility','visible')
     $('#email-popup-background').css('visibility','visible')
@@ -6,4 +11,11 @@ function open_email_form() {
 function close_email_form() {
     $('#email-popup').css('visibility','hidden')
     $('#email-popup-background').css('visibility','hidden')
+}
+
+function change_color_theme() {
+    document.documentElement.style.setProperty('--intellij-dark-gray2', '#cbcbcb');
+    document.documentElement.style.setProperty('--intellij-dark-gray3', '#a6a6a6');
+    document.documentElement.style.setProperty('--intellij-dark-white', '#2A2A2A');
+    document.documentElement.style.setProperty('--intellij-purple', '#4884C1');
 }
