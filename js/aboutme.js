98763c637e6628c55a904814b5c6865c1edc5878
KorbiQWeidinger <korbi.weidinger2@gmail.com>
2022-03-30 19:54:49 +0200

    color themes

--- js/aboutme.js
+++ js/aboutme.js
@@ -13,9 +13,18 @@ function close_email_form() {
     $('#email-popup-background').css('visibility','hidden')
 }
 
-function change_color_theme() {
-    document.documentElement.style.setProperty('--intellij-dark-gray2', '#cbcbcb');
-    document.documentElement.style.setProperty('--intellij-dark-gray3', '#a6a6a6');
-    document.documentElement.style.setProperty('--intellij-dark-white', '#2A2A2A');
-    document.documentElement.style.setProperty('--intellij-purple', '#4884C1');
+function change_color_theme(name) {
+    if(name === 'blue') {
+        document.documentElement.style.setProperty('--intellij-gray2', '#797979');
+        document.documentElement.style.setProperty('--intellij-dark-gray2', '#cbcbcb');
+        document.documentElement.style.setProperty('--intellij-dark-gray3', '#a6a6a6');
+        document.documentElement.style.setProperty('--intellij-dark-white', '#2A2A2A');
+        document.documentElement.style.setProperty('--intellij-purple', '#4884C1');
+    } else {
+        document.documentElement.style.setProperty('--intellij-gray2', '#4C5051');
+        document.documentElement.style.setProperty('--intellij-dark-gray2', '#2A2A2A');
+        document.documentElement.style.setProperty('--intellij-dark-gray3', '#303133');
+        document.documentElement.style.setProperty('--intellij-dark-white', '#bbbbbb');
+        document.documentElement.style.setProperty('--intellij-purple', '#9376AA');
+    }
 }
