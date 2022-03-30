afd3cc5350449da2edd4758f4bedd096ad843e1c
KorbiQWeidinger <korbi.weidinger2@gmail.com>
2022-03-30 17:47:04 +0200

    my website...

---
+++ js/aboutme.js
@@ -0,0 +1,9 @@
+function open_email_form() {
+    $('#email-popup').css('visibility','visible')
+    $('#email-popup-background').css('visibility','visible')
+}
+
+function close_email_form() {
+    $('#email-popup').css('visibility','hidden')
+    $('#email-popup-background').css('visibility','hidden')
+}
