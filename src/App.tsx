ff460167963be5eacd44fbc59199cd895d8fdacd
Korbi <korbi.weidinger2@gmail.com>
2024-12-01 23:56:44 +0100

    feat: coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import GradualSpacing from "./components/ui/gradual-spacing";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <Button>Click me</Button>
+    <div className="flex flex-col space-y-4 justify-center items-center h-screen w-screen">
+      <GradualSpacing
+        className="font-display text-center text-4xl font-bold -tracking-widest  text-black dark:text-white md:text-7xl md:leading-[5rem]"
+        text="Coming Soon"
+      />
     </div>
   );
 }
